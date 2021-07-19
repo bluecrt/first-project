7e21bb0367278cd4a2206c44c6fefbbe52868097
username <email>
2021-07-19 10:34:10 +0800

    all

--- grab/requests4.py
+++ grab/requests4.py
@@ -1,6 +1,7 @@
 import requests
-res = requests.get('https://www.taobao.com')
+res = requests.get('https://www.baidu.com')
 res.encoding = 'utf-8'
-text = res.text
+#text = res.text
 
-print(text)
+#print(text)
+print(res.status_code)
